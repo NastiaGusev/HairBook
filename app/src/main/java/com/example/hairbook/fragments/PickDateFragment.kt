5036936efac4c4fcd603a575ba834d4491084e4a
NastiaGusev <gusev.nastia@gmail.com>
2022-01-27 20:25:59 +0200

    Added HorizontalCalender to pickServices

--- app/src/main/java/com/example/hairbook/fragments/PickDateFragment.kt
+++ app/src/main/java/com/example/hairbook/fragments/PickDateFragment.kt
@@ -1,24 +1,25 @@
 package com.example.hairbook.fragments
 
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
 import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.hairbook.R
+import com.example.hairbook.activities.ServicesActivity
 import com.example.hairbook.data.ManDataSource
 import com.example.hairbook.data.WomanDataSource
-import com.example.hairbook.databinding.FragmentChooseServiceBinding
 import com.example.hairbook.databinding.FragmentPickDateBinding
 import com.example.hairbook.models.ServiceItem
 import com.google.android.material.transition.MaterialContainerTransform
+import devs.mulham.horizontalcalendar.HorizontalCalendar
+import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener
 import java.util.*
-import kotlin.collections.ArrayList
+
 
 class PickDateFragment : Fragment() {
 
@@ -26,7 +27,6 @@ class PickDateFragment : Fragment() {
     private val serviceId: Long by lazy(LazyThreadSafetyMode.NONE) { args.serviceItemId }
     private val serviceGender: String by lazy(LazyThreadSafetyMode.NONE) { args.serviceItemGender }
     private lateinit var service: ServiceItem
-
     private lateinit var binding: FragmentPickDateBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +43,7 @@ class PickDateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         binding = FragmentPickDateBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -60,13 +61,43 @@ class PickDateFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        createCalender()
         binding.pickDateBack.setOnClickListener {
             findNavController().navigateUp()
         }
         addDataSet()
+
         binding.serviceTitle.text = service.serviceName
         binding.serviceImage.setImageResource(service.image)
 
     }
+
+    private fun createCalender(){
+        /* starts before 1 month from now */
+        val startDate: Calendar = Calendar.getInstance()
+        startDate.add(Calendar.DAY_OF_MONTH, 0)
+
+        /* ends after 1 month from now */
+        val endDate: Calendar = Calendar.getInstance()
+        endDate.add(Calendar.MONTH, 2)
+
+        val horizontalCalendar = HorizontalCalendar.Builder(activity, binding.calendarView.id)
+            .range(startDate, endDate)
+            .datesNumberOnScreen(5)
+            .build()
+
+        horizontalCalendar.calendarListener = object : HorizontalCalendarListener() {
+            override fun onDateSelected(date: Calendar?, position: Int) {
+                //do something
+                if (date != null) {
+                    Log.d("Cal1","time: ${date.time}")
+                    Log.d("Cal1","YEAR: ${date.get(Calendar.YEAR)}")
+                    Log.d("Cal1","DAY_OF_MONTH: ${date.get(Calendar.DAY_OF_MONTH)}")
+                    Log.d("Cal1","MONTH: ${date.get(Calendar.MONTH)}")
+                }
+            }
+        }
+
+    }
+
 }
